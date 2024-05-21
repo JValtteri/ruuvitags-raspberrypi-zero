95f3e253d87d5473388387843b3f6b16a7c2e08e
Valtteri Ojala <46611993+JValtteri@users.noreply.github.com>
2024-05-21 20:40:40 +0300

    Update start-collector to point to ruuvi2influx

--- ruuvi-collector/start-collector.sh
+++ ruuvi-collector/start-collector.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-docker run \
+set -xe
+
+sudo docker run \
     -d \
     --name ruuvi \
-    --network grafana-influxdb \
     --restart unless-stopped \
     --net=host \
     --cap-add=NET_ADMIN \
     --mount type=bind,source="$(pwd)"/config.yml,target=/app/config.yml,readonly \
-    ruuvitag-logger-py:deb-v0.2
+    ruuvi2influx:latest
